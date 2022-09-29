afafd6c0e7a54265ea4244da348e30eff770b5fd
Howk <114512169+Howlk@users.noreply.github.com>
2022-09-29 13:48:38 -0400

    Update carrossel.js

--- carrossel.js
+++ carrossel.js
@@ -1,15 +1,16 @@
-const carrs = document.getElementById("cart")
-const carr = document.querySelectorAll("#cart img")
+let slide = document.querySelectorAll('.cr');
+let idx = 0;
 
-let a = 0
-
-function carros(){
-    a++;
-    if(a > carr.length - 1){
-        a = 0;
-    }
-
-    carrs.style.transform = `translateX(${-a * 1000}px)`
+function next(){
+    slide[idx].classList.remove('carr');
+    idx = (idx + 1) % slide.length;
+    slide[idx].classList.add('carr');
 }
 
-setInterval(carros, 3000)
+function ant(){
+    slide[idx].classList.remove('carr');
+    idx = (idx - 1 + slide.length) % slide.length;
+    slide[idx].classList.add('carr');
+}
+
+setInterval(next, 10000);
